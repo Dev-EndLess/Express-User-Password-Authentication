a4bd486eb6e6fe34df43e6615df753add1e0d714
Dev-EndLess <endlesspain1988@gmail.com>
2022-12-09 17:24:43 +0100

    add stuff

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const employeesController = require('../../controllers/employeesController')
-
+const verifyJWT = require('../../middleware/verifyJWT')
 
 router.route('/')
-  .get(employeesController.getAllEmployees)
+  // go to the middleware first and than to employeesColtroller
+  .get(verifyJWT, employeesController.getAllEmployees)
   .post(employeesController.createNewEmployee)
   .put(employeesController.updateEmployee)
   .delete(employeesController.deleteEmployee)
