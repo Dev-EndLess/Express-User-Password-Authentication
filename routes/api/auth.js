e3b07fd75e131806cffc37ae9d4d6cbf179c7289
Dev-EndLess <endlesspain1988@gmail.com>
2022-12-09 17:22:24 +0100

    files moved

--- routes/api/auth.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const registerAuth = require('../../controllers/authController')
-
-router.post('/', registerAuth.handleLogin)
-
-module.exports = router
